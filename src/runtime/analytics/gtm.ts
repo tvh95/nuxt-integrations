e6c5636fc73705a782516e3fdb4a91efacef539f
Vee Han Tan <veehantan@Vees-MacBook-Air.local>
2022-08-30 15:22:21 +0800

    wip

--- src/runtime/analytics/gtm.ts
+++ src/runtime/analytics/gtm.ts
@@ -3,7 +3,19 @@ export const useGtm = () => {
     throw new Error('GTM wasn\'t setup properly. Please check your configurations.')
   }
 
+  function pushEcommerce (event, ecommerce) {
+    window.dataLayer.push({ ecommerce: null })
+    window.dataLayer.push({ event, ecommerce })
+  }
+
+  function pushUser (userId: string, loginType: string = 'email') {
+    const data = userId ? { isLoggedIn: true, user_id: userId, loginType } : { isLoggedIn: false }
+    window.dataLayer.push(data)
+  }
+
   return {
-    dataLayer: window.dataLayer
+    pushDataLayer: window.dataLayer.push,
+    pushEcommerce,
+    pushUser
   }
 }
